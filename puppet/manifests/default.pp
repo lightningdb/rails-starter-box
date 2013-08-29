915e40ba74768fe2916c07be3ece8507f86bd2d2
David Bolton <david@davidbolton.net>
2013-08-29 10:23:47 +1000

    variety of updates

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -27,7 +27,7 @@ ensure => installed,
 
 # RMagick system dependencies
 package { ['libmagickwand4', 'libmagickwand-dev']:
-ensure => installed,
+  ensure => installed,
 }
 
 class install_mysql {
@@ -69,21 +69,16 @@ class install-rvm {
   rvm_system_ruby {
     'ruby-1.9.3-p194':
       ensure => 'present',
-      default_use => false;
-    'ruby-1.8.7-p370':
-      ensure => 'present',
-      default_use => false;
+      default_use => true;
   }
 
   rvm_gem {
     'ruby-1.9.3-p194/bundler': ensure => latest;
     'ruby-1.9.3-p194/rails': ensure => latest;
     'ruby-1.9.3-p194/rake': ensure => latest;
-    'ruby-1.8.7-p370/bundler': ensure => latest;
-    'ruby-1.8.7-p370/rails': ensure => latest;
-    'ruby-1.8.7-p370/rake': ensure => latest;
   }
 
 }
 
 class { 'install-rvm': }
+
